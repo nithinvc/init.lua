2a3113589c0655df00b2203051b664d5272cc9cf
Nithin Chalapathi <nithin.ch10@gmail.com>
2024-11-15 11:30:46 -0800

    lazy setup, most sets, some remaps

--- lua/nithinc/init.lua
+++ lua/nithinc/init.lua
@@ -1,1 +1,5 @@
+require("nithinc.set")
 require("nithinc.remap")
+require("nithinc.lazy_init")
+require("nithinc.cmds")
+
---
+++ lua/nithinc/set.lua
@@ -0,0 +1,49 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.showmode = false
+vim.opt.breakindent = true
+-- Require at least two captial letters or \C for capital letters to matter
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Not sure what this actually does? It just seems to add space left of the line numbers?
+vim.opt.signcolumn = 'yes'
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- Splits? Seems useful but right now I don't even use splits lol
+-- vim.opt.splitright = true
+-- vim.opt.splitbelow = true
+
+-- Displaying whitespace chars
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Live substitutions
+vim.opt.inccommand = 'split'
+
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+
+-- Tab settings
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+-- Swap file and memory settings
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.termguicolors = true
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
