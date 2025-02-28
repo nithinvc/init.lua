4f6df0198059f9d27a41455f0df778d48b563cce
Nithin Chalapathi <nithin.ch10@gmail.com>
2025-02-28 10:33:00 -0800

    new tmux and config

--- configure_tmux.sh
+++ configure_tmux.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 mkdir -p ~/.config/tmux/plugins/catppuccin
-git clone -b v2.1.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+
+if [ ! -n "$(ls -A ~/.config/tmux/plugins/catppuccin/tmux 2>/dev/null)" ]; then
+    git clone -b v2.1.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+fi
 cp tmux.conf ~/.tmux.conf
+tmux source-file ~/.tmux.conf
