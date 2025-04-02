6cd4187707f33bb2984856a015a64ebe37519283
Nithin Chalapathi <nithin.ch10@gmail.com>
2025-04-02 16:57:42 -0700

    add command so that python files dont autoindent when typing a colon

--- lua/nithinc/cmds.lua
+++ lua/nithinc/cmds.lua
@@ -6,3 +6,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "python", -- or any filetype you need
+	callback = function()
+		vim.opt_local.indentkeys = "0{,0},0),0],:,0#,!^F,o,O,e"
+	end,
+})
