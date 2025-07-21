7067d17f8a6a20aa4bb2fc88d7b27e8e6879e0e4
Nithin Chalapathi <nithin.ch10@gmail.com>
2025-07-21 11:32:10 -0700

    turn off tmux auto source since it breaks without tmux running

--- configure_tmux.sh
+++ configure_tmux.sh
@@ -13,4 +13,4 @@ if [ ! -n "$(ls -A ~/.config/tmux/plugins/catppuccin/tmux 2>/dev/null)" ]; then
     git clone -b v2.1.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
 fi
 cp tmux.conf ~/.tmux.conf
-tmux source-file ~/.tmux.conf
+#tmux source-file ~/.tmux.conf
