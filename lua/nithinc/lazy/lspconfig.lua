0f56df202192acecf0af028929f874630d7bba27
Nithin Chalapathi <nithin.ch10@gmail.com>
2026-04-20 18:56:47 -0700

    lsp

--- lua/nithinc/lazy/lspconfig.lua
+++ lua/nithinc/lazy/lspconfig.lua
@@ -94,7 +94,15 @@ return {
 				--  For example, in C this would take you to the header.
 				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-				-- The following two autocommands are used to highlight references of the
+				-- Show signature help after typing ( or ,
+			for _, char in ipairs({ "(", "," }) do
+				vim.keymap.set("i", char, function()
+					vim.api.nvim_feedkeys(char, "n", false)
+					vim.defer_fn(vim.lsp.buf.signature_help, 50)
+				end, { buffer = event.buf })
+			end
+
+			-- The following two autocommands are used to highlight references of the
 				-- word under your cursor when your cursor rests there for a little while.
 				--    See `:help CursorHold` for information about when this is executed
 				--
